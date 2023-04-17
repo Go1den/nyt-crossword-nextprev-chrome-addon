ea72e06b18a22b415c1a99f07a2f7abfbff4ee61
Golden <gooneden@gmail.com>
2023-04-17 02:00:35 -0500

    Early commit

---
+++ content-script.js
@@ -0,0 +1,4 @@
+console.log('NYT Crossword Navigator');
+
+<li class="xwd__tool--button xwd__tool--texty"><button type="button" aria-label="Previous Puzzle">Previous Puzzle</button></li>
+<li class="xwd__tool--button xwd__tool--texty"><button type="button" aria-label="Next Puzzle">Next Puzzle</button></li>
